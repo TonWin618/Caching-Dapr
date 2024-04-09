c9249d0e68dea5ff65db4601199eae06a7c910cb
汤问 <tonwin618@qq.com>
2024-04-09 16:07:23 +0800

    Awaiting testing

--- src/DaprCache.cs
+++ src/DaprCache.cs
@@ -51,17 +51,23 @@ namespace Microsoft.Extensions.Caching.Dapr
 
             token.ThrowIfCancellationRequested();
 
-            var result = await _client.GetStateAsync<ByteString>(_options.StoreName, key, null, null, token);
-            var array = result.ToArray();
-            var (isSlidingExpiration, ttl) = GetCacheHeader(ref array);
+            var extendedValue = await _client.GetStateAsync<ByteString>(_options.StoreName, key, null, null, token);
+            var extendedValueString  = extendedValue.ToString();
+
+            if(extendedValueString == null)
+            {
+                return null;
+            }
+
+            var (isSlidingExpiration, ttlInSeconds, realValue) = CacheSerializationHelper.DeserializeCacheValue(extendedValueString);
 
             if (isSlidingExpiration)
             {
-                var options = CacheTtlCalculateHelper.ToSlidingExpirationOption(ttl);
+                var options = CacheTtlCalculateHelper.ToSlidingExpirationOption(ttlInSeconds);
                 await SetAsync(key, [], options);
             }
 
-            return result.Skip(1).ToArray();
+            return realValue;
         }
 
         public void Refresh(string key)
@@ -80,7 +86,7 @@ namespace Microsoft.Extensions.Caching.Dapr
 
             if (await _client.CheckHealthAsync(token))
             {
-                await _client.GetMetadataAsync();
+                _ = await GetAsync(key, token);
             }
             else
             {
@@ -134,49 +140,17 @@ namespace Microsoft.Extensions.Caching.Dapr
             if (await _client.CheckHealthAsync(token))
             {
                 var (isSlidingExpiration, ttl) = CacheTtlCalculateHelper.CalculateTtlSeconds(options);
-                SetCacheHeader(ref value, isSlidingExpiration, ttl);
+                var valueString = CacheSerializationHelper.SerilizeCacheValue(isSlidingExpiration, ttl, value);
 
                 var metadata = new Dictionary<string, string>();
                 metadata.Add("ttlInSeconds", ttl.ToString());
 
-                await _client.SaveStateAsync(_options.StoreName, key, value, null, metadata, token);
+                await _client.SaveStateAsync(_options.StoreName, key, valueString, null, metadata, token);
             }
             else
             {
                 throw new InvalidOperationException("sidecar is unhealthy.");
             }
-        }
-
-        private static void SetCacheHeader(ref byte[] array, bool isSlidingExpiration, int ttl)
-        {
-            byte[] newArray = new byte[array.Length + 5];
-
-            byte[] boolBytes = BitConverter.GetBytes(isSlidingExpiration);
-            byte[] intBytes = BitConverter.GetBytes(ttl);
-            byte[] combinedBytes = new byte[5];
-
-            Buffer.BlockCopy(boolBytes, 0, combinedBytes, 0, 1);
-            Buffer.BlockCopy(intBytes, 0, combinedBytes, 1, 4);
-
-            Array.Copy(combinedBytes, 0, newArray, 0, 5);
-            Array.Copy(array, 0, newArray, 5, array.Length);
-
-            array = newArray;
-        }
-
-        private static (bool,int) GetCacheHeader(ref byte[] array)
-        {
-            byte[] result = new byte[5];
-            Array.Copy(array, 0, result, 0, 5);
-
-            bool isSlidingExpiration = BitConverter.ToBoolean(result, 0);
-            int ttl = BitConverter.ToInt32(result, 1);
-
-            byte[] newArray = new byte[array.Length - 5];
-            Array.Copy(array, 5, newArray, 0, array.Length - 5);
-            array = newArray;
-
-            return (isSlidingExpiration,ttl);
         }
     }
 }
